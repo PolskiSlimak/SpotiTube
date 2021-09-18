88d9029524626883ab58ab1c41412055f552c254
Kamil <slimak-75@wp.pl>
2021-09-18 17:56:19 +0200

    możliwość zestawnia utworów z różnych playlist

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -9,8 +9,9 @@ import {Router} from '@angular/router';
 })
 export class MainPageComponent implements OnInit {
 
-  playlistInfo = [];
-  trackInfo = [];
+  playlistInfo: any = [];
+  tracksInfo: any = [];
+  trackList: any = [];
 
   constructor(private router: Router,
               private spotifyService: SpotifyService) {
@@ -27,16 +28,60 @@ export class MainPageComponent implements OnInit {
       if (items) {
         this.playlistInfo = items;
       }
-      console.log(data);
     });
   }
 
-  showMusic(item: any): any {
+  showMusic(item: any): void {
     let playlistId = item.id;
     this.spotifyService.getTracks(playlistId).subscribe((data: any) => {
-      this.trackInfo = data.items;
-      console.log(this.trackInfo)
+      this.addTracks(data.items, playlistId);
     });
+  }
+
+  addTracks(items: any, playlistId: number): void {
+    let trackInfo = {items: items, playlistId: playlistId};
+    let index = this.tracksInfo.findIndex((element: any) => {
+      return element.playlistId === trackInfo.playlistId;
+    });
+    if (index > -1) {
+      this.tracksInfo.splice(index, 1);
+      this.deleteRelatedTracks(trackInfo.items);
+    } else {
+      this.tracksInfo.push(trackInfo);
+      this.updateShownTracks(trackInfo.items);
+    }
+  }
+
+  updateShownTracks(itemTracks: any): void {
+    itemTracks.forEach((newTrack: any) => {
+      let isAlreadyExist = this.trackList.some((alreadyAddedTrack: any) => {
+        return alreadyAddedTrack.track.id === newTrack.track.id;
+      });
+      if (!isAlreadyExist) {
+        this.trackList.push(newTrack);
+      }
+    });
+  }
+
+  deleteRelatedTracks(itemTracks: any): void {
+    itemTracks.forEach((newTrack: any) => {
+      let index = this.trackList.findIndex((alreadyAddedTrack: any) => {
+        return alreadyAddedTrack.track.id === newTrack.track.id;
+      });
+      if (index > -1 && !this.checkIfContainsInDiffrentPlaylist(newTrack)) {
+        this.trackList.splice(index, 1);
+      }
+    });
+  }
+
+  checkIfContainsInDiffrentPlaylist(track: any): boolean {
+    let exists = false;
+    let isAlreadyExist = this.tracksInfo.forEach((info: any) => {
+      exists = info.items.some((alreadyAddedTrack: any) => {
+        return alreadyAddedTrack.track.id === track.track.id;
+      });
+    });
+    return exists;
   }
 
   // getPlaylistNames(items: Object[]): string[] {
